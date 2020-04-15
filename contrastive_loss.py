c7520ad53d4c0353c11609c542dccdaa48421149
hahaha <fancui@mail.nwpu.edu.cn>
2020-04-15 11:24:08 +0800

    Update contrastive_loss.py

--- contrastive_loss.py
+++ contrastive_loss.py
@@ -5,13 +5,13 @@ def contrastive_loss(out,out_aug,batch_size=128,hidden_norm=False,temperature=1.
     if hidden_norm:
         out=tf.nn.l2_normalize(out,-1)
         out_aug=tf.nn.l2_normalize(out_aug,-1)
-    INF = np.inf
+    inf_value = np.inf
     labels = tf.one_hot(tf.range(batch_size), batch_size * 2) #[batch_size,2*batch_size]
     masks = tf.one_hot(tf.range(batch_size), batch_size) #[batch_size,batch_size]
     logits_aa = tf.matmul(out, out, transpose_b=True) / temperature #[batch_size,batch_size]
     logits_bb = tf.matmul(out_aug, out_aug, transpose_b=True) / temperature #[batch_size,batch_size]
-    logits_aa = logits_aa - masks * INF # remove the same samples in out
-    logits_bb = logits_bb - masks * INF # remove the same samples in out_aug
+    logits_aa = logits_aa - masks * inf_value # remove the same samples in out
+    logits_bb = logits_bb - masks * inf_value # remove the same samples in out_aug
     logits_ab = tf.matmul(out, out_aug, transpose_b=True) / temperature
     logits_ba = tf.matmul(out_aug, out, transpose_b=True) / temperature
     loss_a = tf.losses.softmax_cross_entropy(
